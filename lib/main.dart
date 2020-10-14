4f7e1835fe8143178f661a6c27297edc54032590
Vladimir Tanakov <tanakov.dev@gmail.com>
2020-10-14 10:52:44 +0300

    Migrate to new firebase crashlytics sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,29 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sweaterweather/app_starter.dart';
 import 'package:sweaterweather/app_router.dart';
+import 'package:sweaterweather/app_starter.dart';
 import 'package:sweaterweather/ui/widgets/weather_icon_loader.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 
-void main() {
-  Crashlytics.instance.enableInDevMode = !kReleaseMode;
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  }
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
   SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent));
 
   runZoned(() {
     runApp(SplashApp());
-  }, onError: Crashlytics.instance.recordError);
+  }, onError: FirebaseCrashlytics.instance.recordError);
 }
 
 class MainApp extends StatelessWidget {
